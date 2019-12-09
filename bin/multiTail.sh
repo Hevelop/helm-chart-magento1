020a75ca61bf15b100e6049a149b4bd7eb7db696
Simone Marcato <simone@hevelop.com>
2019-12-09 16:34:25 +0100

    different mount path

--- bin/multiTail.sh
+++ bin/multiTail.sh
@@ -4,10 +4,11 @@ set -ex
 paths=$@
 command="";
 
-for path in $paths
+for path in ${paths}
 do
-    for file in $path/*
+    for file in ${path}
     do
+        touch ${file}
         command="${command}tail -f ${file} | sed \"s/^/[${fileescaped}]/\" & "
     done;
 done;
